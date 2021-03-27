ae81589cc45ffefe9acda857851c44d8ae1618e5
dmrompav <dm.rom.pav@gmail.com>
2021-03-27 12:16:38 +0500

    cors problem solving

--- src/App.vue
+++ src/App.vue
@@ -91,9 +91,9 @@ export default {
 			this.isDataTypeLoading = true
 			let requestURL
 			if(value === "small") {
-				requestURL = "https://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}"
+				requestURL = "http://cors-anywhere.herokuapp.com/http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}"
 			} else {
-				requestURL = "https://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}"
+				requestURL = "http://cors-anywhere.herokuapp.com/http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}"
 			}
 			this.dataLoad(requestURL)
 				.then(data => {
